ee6c6947fe876d2e43e1138dfb2fe9630460ab50
amezich <andrew.mezich@colorado.edu>
2016-04-27 13:26:04 -0600

    everything

--- Baseball.cpp
+++ Baseball.cpp
@@ -1,12 +1,14 @@
 #include "Baseball.h"
 #include <string>
 #include <iostream>
-
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 Baseball::Baseball()
 {
-    //ctor
+    headA = NULL;
+    headB = NULL;//ctor
 }
 
 Baseball::~Baseball()
@@ -14,23 +16,339 @@ Baseball::~Baseball()
     //dtor
 }
 
-void Baseball::scorecard()
+int Baseball::mainMenu()
+{   int option = 0;
+    cout << "Main Menu" << endl;
+    cout << "1: Start new scorecard" << endl;
+    cout << "2: Simulate Game" << endl;
+    cout << "3: Quit" << endl;
+    cin >> option;
+    return option;
+}
+
+void Baseball::buildLineups()
 {
     cout << "New Score Card" << endl;
     teamA();
+    batter *tmpA = headA;
+    cout << "Line up for team A:" << endl;
+    int i = 1;
+    while(tmpA->next != NULL)
+    {
+        tmpA = tmpA->next;
+        cout << "Batter " << i << ": #";
+        cout << tmpA->number << " Average: " << tmpA->avg << endl;
+        i++;
+    }
+    teamB();
+    batter *tmpB = headB;
+    cout << "Line up for team B:" << endl;
+    int j = 1;
+    while(tmpB->next != NULL)
+    {
+        tmpB = tmpB->next;
+        cout << "Batter " << j << ": #";
+        cout << tmpB->number << " Average: " << tmpA->avg << endl;
+        j++;
+    }
 }
 
 void Baseball::teamA()
 {
+    //srand (static_cast <unsigned> (time(0)));
+    int num = 0;
+    headA = new batter;
+    batter *current = headA;
     cout << "Please enter line up for team A (player numbers)" << endl;
-    for(int i = 0; i < 9; i++)
+    for(int i = 0; i < 9; ++i)
     {
-        int num = 0;
-        batter *current = new batter;
-        cout << "Batter " << i+1 << endl;
-        cout << "enter player number: ";
-        cin >> num;
-        current->number = num;
-        current->next = NULL;
+        bool valid = false;
+        int j = i;
+        cout << "Batter " << j+1 << endl;
+        while(!valid)
+        {
+            valid = true;
+            cout << "enter player number: ";
+            cin >> num;
+            if(cin.fail())
+            {
+                cin.clear();
+                cin.ignore();
+                cout << "Please enter an Integer only." << endl;
+                valid = false;
+            }
+        }
+        batter *tmp = new batter;
+        tmp->number = num;
+        tmp->avg = 0;
+        tmp->atbats = 0;
+        //float avg = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        //tmp->avg = avg/2;
+        tmp->hits = 0;
+        current->next = tmp;
+        current = tmp;
+
     }
+}
+
+void Baseball::teamB()
+{
+    //srand (static_cast <unsigned> (time(0)));
+    int num = 0;
+    headB = new batter;
+    batter *current = headB;
+    cout << "Please enter line up for team B (player numbers)" << endl;
+    for(int i = 0; i < 9; ++i)
+    {
+        bool valid = false;
+        int j = i;
+        cout << "Batter " << j+1 << endl;
+        while(!valid)
+        {
+            valid = true;
+            cout << "enter player number: ";
+            cin >> num;
+            if(cin.fail())
+            {
+                cin.clear();
+                cin.ignore();
+                cout << "Please enter an Integer only." << endl;
+                valid = false;
+            }
+        }
+        batter *tmp = new batter;
+        tmp->number = num;
+        tmp->avg = 0;
+        tmp->atbats = 0;
+        //float avg = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        //tmp->avg = avg/2;
+        tmp->hits = 0;
+        current->next = tmp;
+        current = tmp;
+    }
+}
+
+void Baseball::scorecard()
+{
+    cout << "PLAY BALL!" << endl;
+    bool top;
+    int inning = 1;
+    int scoreA = 0;
+    int scoreB = 0;
+    batter *leadoffA = headA->next;
+    batter *leadoffB = headB->next;
+    while(inning <= 9)
+    {
+        //team A at bat first
+        top = true;
+        inningAnnouncement(inning, top);
+        batter *currentA = leadoffA;
+        scoreA = keepScore(scoreA,currentA,top);
+
+        //team B at bat second
+        top = false;
+        inningAnnouncement(inning, top);
+        batter *currentB = leadoffB;
+        scoreB = keepScore(scoreB,currentB, top);
+
+        inning++;
+    }
+    cout << "Game Over" << endl;
+    cout << "Final Score: " << endl;
+    cout << "Team A - " << scoreA << endl;
+    cout << "Averages: " << endl;
+    batter *tmpA = headA;
+    cout << "Line up for team A:" << endl;
+    int i = 1;
+    while(tmpA->next != NULL)
+    {
+        tmpA = tmpA->next;
+        cout << "Batter " << i << ": #";
+        cout << tmpA->number << " Average: " << tmpA->avg << endl;
+        i++;
+    }
+    cout << "Team B - " << scoreB << endl;
+    cout << "Averages: " << endl;
+    batter *tmpB = headB;
+    cout << "Line up for team B:" << endl;
+    int j = 1;
+    while(tmpB->next != NULL)
+    {
+        tmpB = tmpB->next;
+        cout << "Batter " << j << ": #";
+        cout << tmpB->number << " Average: " << tmpA->avg << endl;
+        j++;
+    }
+}
+
+void Baseball::inningAnnouncement(int inning,bool top)
+{
+    string half = "Top";
+    string team = "A";
+    if(!top)
+    {
+        half = "Bottom";
+        team = "B";
+    }
+    cout << "Team " << team << " at bat" << endl;
+    if(inning == 1)
+    {
+        cout << half << " of the " << inning << "st inning" << endl;
+    }
+    else if(inning == 2)
+    {
+        cout << half << " of the " << inning << "nd inning" << endl;
+    }
+    else if(inning == 3)
+    {
+        cout << half << " of the " << inning << "rd inning" << endl;
+    }
+    else
+    {
+        cout << half << " of the " << inning << "th inning" << endl;
+    }
+}
+
+int Baseball::keepScore(int score,batter *current, bool top)
+{
+    int outs = 0;
+    int first = 0;
+    int second = 0;
+    int third = 0;
+    int atBat;
+
+    cout << "Please enter the results of each at bat: " << endl;
+    int hit = 0;
+    while(outs != 3)
+    {
+        cout << "Batter #" << current->number << ":" << endl;
+        cout << "0: out" << endl;
+        cout << "1: single" << endl;
+        cout << "2: double" << endl;
+        cout << "3: triple" << endl;
+        cout << "4: home run" << endl;
+        cin >> atBat;
+        //int hit = 0;
+        //atBat = rand()%5;
+        //hit = current->avg+static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        //atBat = atBat * (rand()%4 +1);
+        if(atBat != 0)
+        {
+            hit = 1;
+        }
+        calcAvg(current,hit);
+        if(atBat == 0)
+        {
+            cout << "Batter #" << current->number << " is out" << endl;
+            outs++;
+            cout << outs << " out(s)" << endl;
+        }
+        if(atBat == 1)
+        {
+            cout << "Batter #" << current->number << " singled" << endl;
+            if(third == 1)
+            {
+                score++;
+                third = 0;
+            }
+            if(second == 1)
+            {
+                third = 1;
+                second = 0;
+            }
+            if(first == 1)
+            {
+                second = 1;
+            }
+            first = 1;
+        }
+        else if(atBat == 2)
+        {
+            cout << "Batter #" << current->number << " doubled" << endl;
+            if(third == 1)
+            {
+                score++;
+                third = 0;
+            }
+            if(second == 1)
+            {
+                score++;
+                second = 0;
+            }
+            if(first == 1)
+            {
+                third = 1;
+            }
+            second = 1;
+            first = 0;
+        }
+        else if(atBat == 3)
+        {
+            cout << "Batter #" << current->number << " tripled" << endl;
+            if(third == 1)
+            {
+                score++;
+                third = 0;
+            }
+            if(second == 1)
+            {
+                score++;
+                second = 0;
+            }
+            if(first == 1)
+            {
+                score++;
+                first = 0;
+            }
+            first = 0;
+            second = 0;
+            third = 1;
+        }
+        else if(atBat == 4)
+        {
+            cout << "Batter #" << current->number << " homered" << endl;
+            if(third == 1)
+            {
+                score++;
+                third = 0;
+            }
+            if(second == 1)
+            {
+                score++;
+                second = 0;
+            }
+            if(first == 1)
+            {
+                score++;
+                first = 0;
+            }
+            first = 0;
+            second = 0;
+            third = 0;
+        }
+        if(current->next == NULL)
+        {
+            if(top)
+            {
+                current = headA->next;
+            }
+            else
+            {
+                current = headB->next;
+            }
+
+        }
+        else
+        {
+            current = current->next;
+        }
+    }
+    return score;
+}
+
+void Baseball::calcAvg(batter *player, int hit)
+{
+    player-> hits = player -> hits + hit;
+    player-> atbats = player->atbats + 1;
+    player -> avg = (player->hits)/(player->atbats);
 }
--- Baseball.h
+++ Baseball.h
@@ -6,9 +6,9 @@ using namespace std;
 
 struct batter
 {
-    int team;
     int number;
-    int avg;
+    int hits;
+    float avg;
     int slg;
     int obp;
     int rbi;
@@ -17,7 +17,10 @@ struct batter
     int b2;
     int b3;
     int b4;
-    bool inthehole = false;
+    int bt;
+    int atbats;
+    bool ondeck = false;
+    batter *previous;
     batter *next;
 };
 class Baseball
@@ -25,13 +28,22 @@ class Baseball
     public:
         Baseball();
         ~Baseball();
+        int mainMenu();
         void scorecard();
-    protected:
-    private:
+        batter *halfInning(batter *current);
+        void buildLineups();
         void teamA();
         void teamB();
-        batter *leadoffA;
-        batter *leadoffB;
+        void inningAnnouncement(int inning, bool top);
+        int keepScore(int score,batter*current, bool top);
+        void calcAvg(batter *player, int hit);
+        void calcOBP();
+        void calcSLG();
+    protected:
+    private:
+        batter *headA;
+        batter *headB;
+        batter *up;
 };
 
 #endif // BASEBALL_H
