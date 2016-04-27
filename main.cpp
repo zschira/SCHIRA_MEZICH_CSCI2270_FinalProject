d48405f7c4e4cff6c40343f62f380e13489e92b6
amezich <andrew.mezich@colorado.edu>
2016-04-27 13:15:08 -0600

    updated main

--- main.cpp
+++ main.cpp
@@ -2,15 +2,7 @@
 #include "Baseball.h"
 using namespace std;
 
-int mainMenu()
-{   int option = 0;
-    cout << "Main Menu" << endl;
-    cout << "1: Start new scorecard" << endl;
-    cout << "2: Simulate Game" << endl;
-    cout << "3: Quit" << endl;
-    cin >> option;
-    return option;
-}
+
 int main()
 {
     Baseball B;
@@ -18,11 +10,12 @@ int main()
     int option = 0;
     while(option != 3)
     {
-        option = mainMenu();
+        option = B.mainMenu();
         switch(option)
         {
         case 1:
             {
+                B.buildLineups();
                 B.scorecard();
             }break;
         case 2:
